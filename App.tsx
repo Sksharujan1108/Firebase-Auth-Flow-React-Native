b30f702fa4c74c9c3ea3aa6fc70aabc1fbbe62ea
sarujan1 <sarujan@codelantic.com>
2024-12-20 16:10:47 +0530

    update

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native';
 import AppRoutes from './src/navigation/routes';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <AppRoutes/>
+      <GestureHandlerRootView style = {{ flex: 1 }}>
+        <SafeAreaProvider>
+          <AppRoutes/>
+        </SafeAreaProvider>
+      </GestureHandlerRootView>
     </View>
   );
 }
